eb571e4b48d35968c7ee60d1c48a2a628fcffd72
MikeJiahaoXu <mikeke.xu@mail.utoronto.ca>
2022-10-08 18:01:14 -0400

    work finished

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,15 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public boolean addDrivable(String id, Drivable obj){
+        if (!this.drivable_map.containsKey(id)){
+            this.drivable_map.put(id, obj);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 
 
@@ -37,6 +46,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed){
+        for(Drivable i : this.drivable_map.values()){
+            if (i.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,6 +63,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List getTradable(){
+        List l = new ArrayList();
+        for(Drivable i : this.drivable_map.values()){
+            if(i instanceof Tradable) {
+                l.add(i);
+            }
+        }
+        return l;
+    }
 
 
 
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,21 @@
  */
 
 import java.util.List;
+public class DrivableTrader extends Trader{
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money){
+        super(money);
+    }
+
+    public int getSellingPrice(Drivable obj) {
+        if(obj instanceof Tradable){
+            return ((Tradable) obj).getPrice() + obj.getMaxSpeed();
+        }
+        else{
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
